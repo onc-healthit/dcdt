79fe3fdc7c3764fc084b09a9683b5342dd39bad0
joe lamy <joe.lamy@nitorgroup.com>
2012-08-27 13:10:54 +0000

    Fixed issue 29: https://code.google.com/p/modular-spec-phase-3/issues/detail?id=29 Made certs and email addresses configurable.

--- src/main/java/gov/onc/decrypt/LookupTest.java
+++ src/main/java/gov/onc/decrypt/LookupTest.java
@@ -3,6 +3,8 @@
  */
 package gov.onc.decrypt;
 
+import gov.onc.startup.ConfigInfo;
+
 import java.util.HashMap;
 
 /**
@@ -13,172 +15,8 @@ import java.util.HashMap;
 public class LookupTest {
 	
 	// Initialize the lookup HashMap with all the tests and info for each success/failure path
-	private static final HashMap<String, LookupTest> lookupTests = new HashMap<String, LookupTest>();
-	static{
-//		lookupTests.put("joe.lamy@nitorgroup.com",
-//				new LookupTest("joe.lamy@nitorgroup.com", "Joe test",
-//					// Good certs
-//					new LookupTestCertInfo("nitor", "Passed"),	// These will match one test email
-//					// Bad certs
-//					new LookupTestCertInfo[] {
-//						new LookupTestCertInfo("nitor3",	// Just copies of the nitor certs
-//							"Used the nitor2 cert"),
-//						new LookupTestCertInfo("nitor2",	// These will match one test email
-//								"Used the nitor2 cert")
-//					}
-//			));
-		lookupTests.put("dts500@direct1.testteam.us",
-			new LookupTest("dts500@direct1.testteam.us", "DTS 500",
-				// Good certs
-				new LookupTestCertInfo("dts500_valid_cert_record", "Passed"),
-				// Bad certs
-				new LookupTestCertInfo[] {
-					new LookupTestCertInfo("othercert",
-						"This is not the correct address. It is for othercert@direct1.testteam.us"),
-					new LookupTestCertInfo("dts501_valid",
-						"Should have chosen the address bound first"),
-					new LookupTestCertInfo("direct9.testteam.us",
-						"Wrong domain and should have chosen the address bound certificate first"),
-					new LookupTestCertInfo("dts500_valid_ldap",
-						"Should have chosen the address bound certificate first"),
-					new LookupTestCertInfo("dts501_valid_ldap", 
-						"Should have chosen the address bound certificate first")
-				}
-		));
-		lookupTests.put("dts501@direct1.testteam.us",
-				new LookupTest("dts501@direct1.testteam.us", "DTS 501",
-					// Good certs
-					new LookupTestCertInfo("dts501_valid", "Passed"),
-					// Bad certs
-					new LookupTestCertInfo[] {
-						new LookupTestCertInfo("dts501_expired",
-							"Expired Certificate"),
-						new LookupTestCertInfo("direct9.testteam.us",
-							"Wrong domain"),
-						new LookupTestCertInfo("dts501_valid_ldap", 
-							"Your system chose a domain-bound certificate stored in an LDAP instance. Your system should have chosen the DNS CERT domain-bound certificate first")
-					}
-			));
-		lookupTests.put("dts502@direct1.testteam.us",
-				new LookupTest("dts502@direct1.testteam.us", "DTS 502",
-					// Good certs
-					new LookupTestCertInfo("dts502", "Passed"),
-					// Bad certs
-					new LookupTestCertInfo[] {
-						new LookupTestCertInfo("dts501_valid",
-							"Your system chose a valid domain-bound certificate stored in a CERT resource record. Your system should have chosen the address-bound certificate in the CERT resource record first"),
-						new LookupTestCertInfo("direct9.testteam.us",
-							"Wrong domain and should have chosen the address bound certificate first"),
-						new LookupTestCertInfo("dts501_valid_ldap", 
-							"Your System chose a valid domain-bound certificate stored in an LDAP instance. It should have chosen the address-bound certificate in the DNS first")
-					}
-			));
-		lookupTests.put("dts505@direct2.testteam.us",
-				new LookupTest("dts505@direct2.testteam.us", "DTS 505",
-					// Good certs
-					new LookupTestCertInfo("dts505_mac", "Passed"),
-					// Bad certs
-					new LookupTestCertInfo[] {
-						new LookupTestCertInfo("dts505_expired_cert_record",
-								"Your system chose an expired certificate from a DNS CERT resource record. It should have chosen the address bound certificate in the LDAP server."),
-						new LookupTestCertInfo("dts505_unbound",
-							"This certificate is not bound correctly"),
-						new LookupTestCertInfo("dts515_mac",
-							"Your system chose a valid domain-bound certificate in an LDAP server, but it should have used the valid address-bound certificate in the LDAP server.")
-					}
-			));
-		lookupTests.put("dts515@direct2.testteam.us",
-				new LookupTest("dts515@direct2.testteam.us", "DTS 515",
-					// Good certs
-					new LookupTestCertInfo("dts515_mac", "Passed"),
-					// Bad certs
-					new LookupTestCertInfo[] {
-						new LookupTestCertInfo("dts515_address_bound",
-							"Your system chose an address-bound certificate that was stored in a lower priority SRV record LDAP server. It SHOULD have chosen the domain-bound certificate in the higher priority SRV record first, though this is only recommended and not required.")
-					}
-			));
-		lookupTests.put("dts506@direct2.testteam.us",
-				new LookupTest("dts506@direct2.testteam.us", "DTS 506",
-					// Good certs
-					new LookupTestCertInfo("dts506_ldap_1_mac", "Passed"),
-					// Bad certs
-					new LookupTestCertInfo[] {
-						new LookupTestCertInfo("dts515_mac",
-							"Your system chose a domain-bound certificate instead of the valid address-bound certificate stored in the LDAP instance first."),
-						new LookupTestCertInfo("dts506_ldap_2",
-								"Your system chose a valid certificate from a lower priority SRV Record. It SHOULD have chosen the valid address-bound certificate in the higher priority SRV Record LDAP instance. This is recommended practice, but not required.")
-					}
-			));
-		lookupTests.put("dts507@direct3.testteam.us",
-				new LookupTest("dts507@direct3.testteam.us", "DTS 507",
-					// Good certs
-					new LookupTestCertInfo("dts507", "Passed"),
-					// Bad certs
-					new LookupTestCertInfo[] {}
-			));
-		lookupTests.put("dts517@direct3.testteam.us",
-				new LookupTest("dts517@direct3.testteam.us", "DTS 517",
-					// Good certs
-					new LookupTestCertInfo("dts517", "Passed"),
-					// Bad certs
-					new LookupTestCertInfo[] {}
-			));
-		lookupTests.put("dts519@direct3.testteam.us",
-				new LookupTest("dts519@direct3.testteam.us", "DTS 519",
-					// Good certs
-					new LookupTestCertInfo("dts519_valid_ldap_2", "Passed"),
-					// Bad certs
-					new LookupTestCertInfo[] {
-						new LookupTestCertInfo("dts519_expired_ldap1_new",
-							"Your system chose an expired certificate. It should have chosen the valid certificate in the lower priority SRV record LDAP instance.")
-					}
-			));
-		lookupTests.put("dts511@direct4.testteam.us",
-				new LookupTest("dts511@direct4.testteam.us", "DTS 511",
-					// Good certs
-					null,
-					// Bad certs
-					new LookupTestCertInfo[] {}
-			));
-		lookupTests.put("dts520@direct5.testteam.us",
-				new LookupTest("dts520@direct5.testteam.us", "DTS 520",
-					// Good certs
-					null,
-					// Bad certs
-					new LookupTestCertInfo[] {
-						new LookupTestCertInfo("dts520_invalid_address_cert",
-							"Shouldn't use this because it's expired"),
-						new LookupTestCertInfo("dts520_invalid_domain_cert",
-								"Shouldn't use this because it's expired"),
-						new LookupTestCertInfo("dts520_invalid_address_ldap",
-								"Shouldn't use this because it's expired"),
-						new LookupTestCertInfo("dts520_invalid_domain_ldap",
-								"Shouldn't use this because it's expired")
-					}
-			));
-		lookupTests.put("dts512@direct6.testteam.us",
-				new LookupTest("dts512@direct6.testteam.us", "DTS 512",
-					// Good certs
-					null,
-					// Bad certs
-					new LookupTestCertInfo[] {
-						new LookupTestCertInfo("dts512_expired_address_cert",
-							"Shouldn't use this because it's expired"),
-						new LookupTestCertInfo("expired_direct6_domain_cert",
-							"Shouldn't use this because it's expired")
-					}
-			));
-		lookupTests.put("dts522@direct7.testteam.us",
-				new LookupTest("dts522@direct7.testteam.us", "DTS 522",
-					// Good certs
-					new LookupTestCertInfo("direct7_valid", "Passed"),
-					// Bad certs
-					new LookupTestCertInfo[] {
-						new LookupTestCertInfo("dts522_expired_address",
-							"Shouldn't use this because it's expired")
-					}
-			));
-	}
+	// Stage one of two stage initialization because it depends on config files.
+	private static HashMap<String, LookupTest> lookupTests = new HashMap<String, LookupTest>();
 
 	// This address tells us what test case we are dealing with
 	private final String destinationAddress;
@@ -201,6 +39,142 @@ public class LookupTest {
 		this.badCertInfoList = badCertInfoList;
 	}
 	
+	// Initialize the lookup HashMap with all the tests and info for each success/failure path
+	// Stage two of two stage initialization because it depends on config files.
+    public static void fillMap() {
+		lookupTests.put(ConfigInfo.getConfigProperty("DTS_500_EMAIL"),
+				new LookupTest(ConfigInfo.getConfigProperty("DTS_500_EMAIL"), "DTS 500",
+					// Good certs
+					new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_1"), "Passed."),
+					// Bad certs
+					new LookupTestCertInfo[] {
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_2"),
+							"This cert is not for the correct address."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_3"),
+							"Should have chosen the address bound first."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_4"),
+							"Wrong domain and should have chosen the address bound certificate first."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_5"),
+							"Should have chosen the address bound certificate first."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_6"), 
+							"Should have chosen the address bound certificate first.")
+					}
+			));
+		lookupTests.put(ConfigInfo.getConfigProperty("DTS_501_EMAIL"),
+				new LookupTest(ConfigInfo.getConfigProperty("DTS_501_EMAIL"), "DTS 501",
+					// Good certs
+					new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_3"), "Passed."),
+					// Bad certs
+					new LookupTestCertInfo[] {
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_7"),
+							"Expired Certificate."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_4"),
+							"Wrong domain."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_6"), 
+							"Your system chose a domain-bound certificate stored in an LDAP instance. Your system should have chosen the DNS CERT domain-bound certificate first.")
+					}
+			));
+		lookupTests.put(ConfigInfo.getConfigProperty("DTS_502_EMAIL"),
+				new LookupTest(ConfigInfo.getConfigProperty("DTS_502_EMAIL"), "DTS 502",
+					// Good certs
+					new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_11"), "Passed."),
+					// Bad certs
+					new LookupTestCertInfo[] {
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_3"),
+							"Your system chose a valid domain-bound certificate stored in a CERT resource record. Your system should have chosen the address-bound certificate in the CERT resource record first."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_4"),
+							"Wrong domain and should have chosen the address bound certificate first."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_6"), 
+							"Your System chose a valid domain-bound certificate stored in an LDAP instance. It should have chosen the address-bound certificate in the DNS first.")
+					}
+			));
+		lookupTests.put(ConfigInfo.getConfigProperty("DTS_505_EMAIL"),
+				new LookupTest(ConfigInfo.getConfigProperty("DTS_505_EMAIL"), "DTS 505",
+					// Good certs
+					new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_14"), "Passed."),
+					// Bad certs
+					new LookupTestCertInfo[] {
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_13"),
+								"Your system chose an expired certificate from a DNS CERT resource record. It should have chosen the address bound certificate in the LDAP server."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_15"),
+							"This certificate is not bound correctly."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_16"),
+							"Your system chose a valid domain-bound certificate in an LDAP server, but it should have used the valid address-bound certificate in the LDAP server.")
+					}
+			));
+		lookupTests.put(ConfigInfo.getConfigProperty("DTS_515_EMAIL"),
+				new LookupTest(ConfigInfo.getConfigProperty("DTS_515_EMAIL"), "DTS 515",
+					// Good certs
+					new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_16"), "Passed."),
+					// Bad certs
+					new LookupTestCertInfo[] {
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_17"),
+							"Your system chose an address-bound certificate that was stored in a lower priority SRV record LDAP server. It SHOULD have chosen the domain-bound certificate in the higher priority SRV record first, though this is only recommended and not required.")
+					}
+			));
+		lookupTests.put(ConfigInfo.getConfigProperty("DTS_506_EMAIL"),
+				new LookupTest(ConfigInfo.getConfigProperty("DTS_506_EMAIL"), "DTS 506",
+					// Good certs
+					new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_18"), "Passed."),
+					// Bad certs
+					new LookupTestCertInfo[] {
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_16"),
+							"Your system chose a domain-bound certificate instead of the valid address-bound certificate stored in the LDAP instance first."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_19"),
+								"Your system chose a valid certificate from a lower priority SRV Record. It SHOULD have chosen the valid address-bound certificate in the higher priority SRV Record LDAP instance. This is recommended practice, but not required.")
+					}
+			));
+		lookupTests.put(ConfigInfo.getConfigProperty("DTS_507_EMAIL"),
+				new LookupTest(ConfigInfo.getConfigProperty("DTS_507_EMAIL"), "DTS 507",
+					// Good certs
+					new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_20"), "Passed."),
+					// Bad certs
+					new LookupTestCertInfo[] {}
+			));
+		lookupTests.put(ConfigInfo.getConfigProperty("DTS_517_EMAIL"),
+				new LookupTest(ConfigInfo.getConfigProperty("DTS_517_EMAIL"), "DTS 517",
+					// Good certs
+					new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_21"), "Passed."),
+					// Bad certs
+					new LookupTestCertInfo[] {}
+			));
+		lookupTests.put(ConfigInfo.getConfigProperty("DTS_511_EMAIL"),
+				new LookupTest(ConfigInfo.getConfigProperty("DTS_511_EMAIL"), "DTS 511",
+					// Good certs
+					null,
+					// Bad certs
+					new LookupTestCertInfo[] {}
+			));
+		lookupTests.put(ConfigInfo.getConfigProperty("DTS_520_EMAIL"),
+				new LookupTest(ConfigInfo.getConfigProperty("DTS_520_EMAIL"), "DTS 520",
+					// Good certs
+					null,
+					// Bad certs
+					new LookupTestCertInfo[] {
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_24"),
+							"Shouldn't use this because it's expired."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_25"),
+								"Shouldn't use this because it's expired."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_26"),
+								"Shouldn't use this because it's expired."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_27"),
+								"Shouldn't use this because it's expired.")
+					}
+			));
+		lookupTests.put(ConfigInfo.getConfigProperty("DTS_512_EMAIL"),
+				new LookupTest(ConfigInfo.getConfigProperty("DTS_512_EMAIL"), "DTS 512",
+					// Good certs
+					null,
+					// Bad certs
+					new LookupTestCertInfo[] {
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_28"),
+							"Shouldn't use this because it's expired."),
+						new LookupTestCertInfo(ConfigInfo.getConfigProperty("CERT_29"),
+							"Shouldn't use this because it's expired.")
+					}
+			));
+	}
+
 	// Returns null if there is no test case for this destination address
 	public static LookupTest findLookupTest(String destAddr) {
 		if (lookupTests.containsKey(destAddr))
