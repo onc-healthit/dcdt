1e4c7265891c6a899e6dc3fa15b46262cf2e5db3
Michal Kotelba <michal.kotelba@esacinc.com>
2013-10-22 19:42:40 -0400

    Purging the default branch for 3.0 development.

--- core/src/main/java/gov/hhs/onc/dcdt/crypto/constants/PemType.java
+++
@@ -1,8 +0,0 @@
-package gov.hhs.onc.dcdt.crypto.constants;
-
-public interface PemType
-{
-	public final static String PKCS8_PRIVATE_KEY = "PRIVATE KEY";
-	
-	public final static String X509_CERTIFICATE = "CERTIFICATE";
-}
