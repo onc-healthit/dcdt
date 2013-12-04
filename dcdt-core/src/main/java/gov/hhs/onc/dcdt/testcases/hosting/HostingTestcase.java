26098adbd571349655a897b1a55eaa502c0f90dc
Elizabeth So <Elizabeth.So@esacinc.com>
2013-12-04 12:47:42 -0500

    - Supports DCDT-97 (write config for all hosting test cases) - Added hosting test case descriptions and possible results messages

--- dcdt-core/src/main/java/gov/hhs/onc/dcdt/testcases/hosting/HostingTestcase.java
+++ dcdt-core/src/main/java/gov/hhs/onc/dcdt/testcases/hosting/HostingTestcase.java
@@ -1,7 +1,30 @@
 package gov.hhs.onc.dcdt.testcases.hosting;
 
-
 import gov.hhs.onc.dcdt.testcases.ToolTestcase;
+import java.util.List;
 
 public interface HostingTestcase extends ToolTestcase<HostingTestcaseResult> {
+    public HostingTestcaseBinding getBinding();
+
+    public void setBinding(HostingTestcaseBinding binding);
+
+    public HostingTestcaseLocation getLocation();
+
+    public void setLocation(HostingTestcaseLocation location);
+
+    public String getDescription();
+
+    public void setDescription(String description);
+
+    public String getInstructions();
+
+    public void setInstructions(String instructions);
+
+    public String getRtm();
+
+    public void setRtm(String rtm);
+
+    public List<String> getSpecifications();
+
+    public void setSpecifications(List<String> specifications);
 }
