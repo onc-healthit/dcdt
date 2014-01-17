f65dacb74f66332b7eaba0caa2b92ab84643997e
Elizabeth So <Elizabeth.So@esacinc.com>
2014-01-17 12:03:32 -0500

    - Supports DCDT-132 (Create LDIF Wrapper) - Made changes requested in code review DCDT-39 including file name changes and some autowiring of test data

--- dcdt-core/src/main/java/gov/hhs/onc/dcdt/ldap/LdapException.java
+++ dcdt-core/src/main/java/gov/hhs/onc/dcdt/ldap/LdapException.java
@@ -2,22 +2,22 @@ package gov.hhs.onc.dcdt.ldap;
 
 import gov.hhs.onc.dcdt.ToolException;
 
-public class LdifException extends ToolException {
+public class LdapException extends ToolException{
     private final static long serialVersionUID = 0L;
 
-    public LdifException() {
+    public LdapException() {
         super();
     }
 
-    public LdifException(Throwable cause) {
+    public LdapException(Throwable cause) {
         super(cause);
     }
 
-    public LdifException(String message) {
+    public LdapException(String message) {
         super(message);
     }
 
-    public LdifException(String message, Throwable cause) {
+    public LdapException(String message, Throwable cause) {
         super(message, cause);
     }
 }
--- dcdt-core/src/test/java/gov/hhs/onc/dcdt/ldap/utils/ToolLdifUtilsUnitTests.java
+++ dcdt-core/src/test/java/gov/hhs/onc/dcdt/ldap/utils/ToolLdifUtilsUnitTests.java
@@ -1,39 +1,50 @@
-package gov.hhs.onc.dcdt.ldap;
+package gov.hhs.onc.dcdt.ldap.utils;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import gov.hhs.onc.dcdt.ldap.LdifException;
 import gov.hhs.onc.dcdt.test.ToolTestNgUnitTests;
 import org.apache.directory.api.ldap.model.exception.LdapException;
 import org.apache.directory.api.ldap.model.exception.LdapInvalidAttributeValueException;
 import org.apache.directory.api.ldap.model.ldif.LdifEntry;
+import org.springframework.beans.factory.annotation.Value;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 @Test(groups = { "dcdt.test.all", "dcdt.test.unit.all", "dcdt.test.unit.ldap.all", "dcdt.test.unit.ldap.ldif" })
-public class LdifWrapperUnitTests extends ToolTestNgUnitTests {
-    private final static String CERT_NAME_VALID_LDAP = "d1_valid_ldap";
-    private final static String CERT_NAME_INVALID_LDAP = "d1_invalid_ldap";
+public class ToolLdifUtilsUnitTests extends ToolTestNgUnitTests {
     private List<LdifEntry> ldifEntries;
     private byte[] ldifData;
+
+    @Value("${dcdt.test.ldap.ldif.cert.name.valid}")
+    private String testCertNameValid;
+
+    @Value("${dcdt.test.ldap.ldif.cert.name.invalid}")
+    private String testCertNameInvalid;
+
+    @Value("${dcdt.test.instance.domain.name}")
+    private String testDomainName;
+
+    @Value("${dcdt.test.ldap.ldif.mail}")
+    private String testMailAttribute;
 
     @Test
     public void testWriteLdifEntries() throws LdapException, LdifException {
         ldifEntries = new ArrayList<>();
-        ldifEntries.add(createLdifEntry(CERT_NAME_VALID_LDAP));
-        ldifEntries.add(createLdifEntry(CERT_NAME_INVALID_LDAP));
+        ldifEntries.add(createLdifEntry(testCertNameValid));
+        ldifEntries.add(createLdifEntry(testCertNameInvalid));
 
-        ldifData = LdifWrapper.writeLdifEntries(ldifEntries);
+        ldifData = ToolLdifUtils.writeLdifEntries(ldifEntries);
         Assert.assertNotEquals(ldifData.length, 0, "Output stream is empty.");
     }
 
     @Test(dependsOnMethods = "testWriteLdifEntries")
     public void testReadLdifEntries() throws LdapException, LdifException {
-        List<LdifEntry> ldifEntriesRead = LdifWrapper.readLdifEntries(ldifData);
+        List<LdifEntry> ldifEntriesRead = ToolLdifUtils.readLdifEntries(ldifData);
         Assert.assertEquals(ldifEntriesRead.size(), 2, "LDIF entry size is incorrect.");
         Assert.assertEquals(ldifEntriesRead, ldifEntries, "LDIF entries are not the same.");
-        assertLdifEntryAttributesMatch(ldifEntriesRead.get(0), CERT_NAME_VALID_LDAP);
-        assertLdifEntryAttributesMatch(ldifEntriesRead.get(1), CERT_NAME_INVALID_LDAP);
+        assertLdifEntryAttributesMatch(ldifEntriesRead.get(0), testCertNameValid);
+        assertLdifEntryAttributesMatch(ldifEntriesRead.get(1), testCertNameInvalid);
     }
 
     private LdifEntry createLdifEntry(String certName) throws LdapException {
@@ -42,8 +53,8 @@ public class LdifWrapperUnitTests extends ToolTestNgUnitTests {
         ldifEntry.addAttribute("objectClass", "top", "inetOrgPerson", "person", "organizationalPerson");
         ldifEntry.addAttribute("cn", certName);
         ldifEntry.addAttribute("sn", certName);
-        ldifEntry.addAttribute("mail", "d1@direct1.direct-test.com");
-        ldifEntry.addAttribute("o", "direct-test.com");
+        ldifEntry.addAttribute("mail", this.testMailAttribute);
+        ldifEntry.addAttribute("o", this.testDomainName);
         return ldifEntry;
     }
 
@@ -52,7 +63,7 @@ public class LdifWrapperUnitTests extends ToolTestNgUnitTests {
         Assert.assertEquals(ldifEntry.get("objectClass").size(), 4, "ObjectClass attribute size is incorrect.");
         Assert.assertEquals(ldifEntry.get("cn").getString(), certName, "CN attribute is incorrect.");
         Assert.assertEquals(ldifEntry.get("sn").getString(), certName, "SN attribute is incorrect.");
-        Assert.assertEquals(ldifEntry.get("mail").getString(), "d1@direct1.direct-test.com", "Mail attribute is incorrect.");
-        Assert.assertEquals(ldifEntry.get("o").getString(), "direct-test.com", "O attribute is incorrect.");
+        Assert.assertEquals(ldifEntry.get("mail").getString(), this.testMailAttribute, "Mail attribute is incorrect.");
+        Assert.assertEquals(ldifEntry.get("o").getString(), this.testDomainName, "O attribute is incorrect.");
     }
 }
