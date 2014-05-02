ddb84dd561c31e48c7827914b0a9db9d63eebb6f
Michal Kotelba <michal.kotelba@esacinc.com>
2014-05-01 23:12:01 -0400

    - Fixes DCDT-218. - Fixed property overriding by using a custom composite PropertySource.

--- dcdt-core/src/main/java/gov/hhs/onc/dcdt/context/impl/ToolPropertySourcesPlaceholderConfigurer.java
+++ dcdt-core/src/main/java/gov/hhs/onc/dcdt/context/impl/ToolPropertySourcesPlaceholderConfigurer.java
@@ -1,25 +1,76 @@
 package gov.hhs.onc.dcdt.context.impl;
 
+import gov.hhs.onc.dcdt.utils.ToolCollectionUtils;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Properties;
+import javax.annotation.Nullable;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
+import org.springframework.core.env.PropertySources;
+import org.springframework.core.env.PropertySourcesPropertyResolver;
+import org.springframework.util.Assert;
 
 public class ToolPropertySourcesPlaceholderConfigurer extends PropertySourcesPlaceholderConfigurer {
-    private Environment env;
+    private class CompositePropertySource extends EnumerablePropertySource<Map<String, Object>> {
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+        public CompositePropertySource(String name, Properties source) {
+            super(name, ((Map) source));
+        }
+
+        @Nullable
+        @Override
+        public Object getProperty(String propName) {
+            return this.getSource().get(propName);
+        }
+
+        @Override
+        public String[] getPropertyNames() {
+            return ToolCollectionUtils.toArray(this.getSource().keySet(), String.class);
+        }
+
+        @Override
+        public Map<String, Object> getSource() {
+            Map<String, Object> source;
+
+            if (ToolPropertySourcesPlaceholderConfigurer.this.env != null) {
+                source = new LinkedHashMap<>();
+
+                if (!ToolPropertySourcesPlaceholderConfigurer.this.envOverride) {
+                    source.putAll(ToolPropertySourcesPlaceholderConfigurer.this.env.getSystemProperties());
+                }
+
+                source.putAll(super.getSource());
+
+                if (ToolPropertySourcesPlaceholderConfigurer.this.envOverride) {
+                    source.putAll(ToolPropertySourcesPlaceholderConfigurer.this.env.getSystemProperties());
+                }
+            } else {
+                source = super.getSource();
+            }
+
+            return source;
+        }
+    }
+
+    private ConfigurableEnvironment env;
     private boolean envOverride;
+    private PropertySources appliedPropSources;
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
         MutablePropertySources propSources = new MutablePropertySources();
 
         try {
-            PropertySource<?> localPropSource = new PropertiesPropertySource(LOCAL_PROPERTIES_PROPERTY_SOURCE_NAME, this.mergeProperties());
+            PropertySource<?> localPropSource = new CompositePropertySource(LOCAL_PROPERTIES_PROPERTY_SOURCE_NAME, this.mergeProperties());
 
             if (this.localOverride) {
                 propSources.addFirst(localPropSource);
@@ -30,29 +81,23 @@ public class ToolPropertySourcesPlaceholderConfigurer extends PropertySourcesPla
             throw new BeanInitializationException("Unable to load properties.", e);
         }
 
-        if (this.env != null) {
-            PropertySource<Environment> envPropSource = new PropertySource<Environment>(ENVIRONMENT_PROPERTIES_PROPERTY_SOURCE_NAME, this.env) {
-                @Override
-                public Object getProperty(String propName) {
-                    return this.source.getProperty(propName);
-                }
-            };
-
-            if (this.envOverride) {
-                propSources.addFirst(envPropSource);
-            } else {
-                propSources.addLast(envPropSource);
-            }
-        }
-
         this.setPropertySources(propSources);
 
-        super.postProcessBeanFactory(beanFactory);
+        this.processProperties(beanFactory, new PropertySourcesPropertyResolver(propSources));
+
+        this.appliedPropSources = propSources;
+    }
+
+    @Override
+    public PropertySources getAppliedPropertySources() throws IllegalStateException {
+        Assert.state((this.appliedPropSources != null), "Property sources have not been applied yet.");
+
+        return this.appliedPropSources;
     }
 
     @Override
     public void setEnvironment(Environment env) {
-        this.env = env;
+        this.env = ((ConfigurableEnvironment) env);
 
         super.setEnvironment(env);
     }
