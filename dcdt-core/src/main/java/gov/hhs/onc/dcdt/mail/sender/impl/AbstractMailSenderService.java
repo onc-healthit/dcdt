e30de60569e231b3d123818970614802c8773b11
INTRANET\ssavarala <ssavarala@ainq.com>
2019-06-14 09:19:51 -0400

    send a custom results email using gmail

--- dcdt-core/src/main/java/gov/hhs/onc/dcdt/mail/sender/impl/AbstractMailSenderService.java
+++ dcdt-core/src/main/java/gov/hhs/onc/dcdt/mail/sender/impl/AbstractMailSenderService.java
@@ -1,8 +1,35 @@
 package gov.hhs.onc.dcdt.mail.sender.impl;
 
+import java.net.SocketTimeoutException;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nullable;
+import javax.annotation.Resource;
+import javax.mail.AuthenticationFailedException;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
+import org.springframework.core.annotation.Order;
+
 import com.sun.mail.smtp.SMTPSendFailedException;
 import com.sun.mail.smtp.SMTPTransport;
 import com.sun.mail.util.MailConnectException;
+
 import gov.hhs.onc.dcdt.beans.impl.AbstractToolBean;
 import gov.hhs.onc.dcdt.discovery.BindingType;
 import gov.hhs.onc.dcdt.dns.lookup.DnsNameService;
@@ -18,24 +45,6 @@ import gov.hhs.onc.dcdt.mail.utils.ToolMailSessionUtils;
 import gov.hhs.onc.dcdt.net.SslType;
 import gov.hhs.onc.dcdt.utils.ToolArrayUtils;
 import gov.hhs.onc.dcdt.utils.ToolCollectionUtils;
-import java.net.SocketTimeoutException;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Properties;
-import javax.annotation.Nonnegative;
-import javax.annotation.Nullable;
-import javax.annotation.Resource;
-import javax.mail.AuthenticationFailedException;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import org.apache.commons.lang3.ArrayUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.AnnotationAwareOrderComparator;
-import org.springframework.core.annotation.Order;
 
 public abstract class AbstractMailSenderService extends AbstractToolBean implements MailSenderService {
     @FunctionalInterface
@@ -129,19 +138,71 @@ public abstract class AbstractMailSenderService extends AbstractToolBean impleme
         }
 
         try {
-            transport.sendMessage(mailInfo.getMessage(), ArrayUtils.toArray(toAddr.toInternetAddress(mailInfo.getEncoding())));
+       //     transport.sendMessage(mailInfo.getMessage(), ArrayUtils.toArray(toAddr.toInternetAddress(mailInfo.getEncoding())));
+        	
+        	 String to = mailInfo.getTo().toString();
+
+             // Sender's email ID needs to be mentioned
+             String from = "sut.example@gmail.com";
+             final String username = "sut.example@gmail.com";//change accordingly
+             final String password = "smtptesting123";//change accordingly
+
+        
+             String host = "smtp.gmail.com";
+
+             Properties props = new Properties();
+             props.put("mail.smtp.auth", "true");
+             props.put("mail.smtp.starttls.enable", "true");
+             props.put("mail.smtp.host", host);
+             props.put("mail.smtp.port", "25");
+
+             // Get the Session object.
+             Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                   protected PasswordAuthentication getPasswordAuthentication() {
+                      return new PasswordAuthentication(username, password);
+       	   }
+                });
+          
+            
+             // Create a default MimeMessage object.
+      	   Message message = new MimeMessage(session);
+      	
+      	   // Set From: header field of the header.
+      	   message.setFrom(new InternetAddress(from));
+      	
+      	   // Set To: header field of the header.
+      	   message.setRecipients(Message.RecipientType.TO,
+                     InternetAddress.parse(to));
+      	
+      	   // Set Subject: header field
+      	   message.setSubject(mailInfo.getMessage().getSubject());
+    
+      	 
+      	 
+      	 message.setContent(mailInfo.getMessage().getContent(), mailInfo.getMessage().getContentType());
+
+      	   // Send message
+      	   Transport.send(message);
+
+      	   System.out.println("Sent message successfully....");
         } catch (SMTPSendFailedException e) {
             throw new ToolMailException(
                 String.format(
                     "Mail MIME message (id=%s, from=%s, to=%s) sender service transport (protocol=%s) connection (heloName=%s, host=%s, port=%d, from=%s, to=%s) send command (%s) failed (resultCode=%d).",
                     mailInfo.getMessageId(), mailInfo.getFrom(), mailInfo.getTo(), transport.getProtocol().getId(), heloName, transport.getHost(),
                     transport.getPort(), fromAddr, toAddr, e.getCommand(), e.getReturnCode()), e);
+      
+        	
         } catch (MessagingException e) {
             throw new ToolMailException(
                 String.format(
                     "Mail MIME message (id=%s, from=%s, to=%s) sender service transport (protocol=%s) connection (heloName=%s, host=%s, port=%d, from=%s, to=%s) send failed.",
                     mailInfo.getMessageId(), mailInfo.getFrom(), mailInfo.getTo(), transport.getProtocol().getId(), heloName, transport.getHost(),
                     transport.getPort(), fromAddr, toAddr), e);
+            
+        } catch (Exception e) {
+        	
         } finally {
             try {
                 transport.close();
@@ -182,7 +243,7 @@ public abstract class AbstractMailSenderService extends AbstractToolBean impleme
 
         try {
             transport.connect(host, port, user, pass);
-
+            System.out.println("DEBUGGER**-->"+" "+host+" "+port+" "+user+" "+pass);
             return transport;
         } catch (Exception e) {
             if ((e instanceof MailConnectException) && (e.getCause() instanceof SocketTimeoutException)) {
